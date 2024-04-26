a0dbfb0542691b2d343e3dd65dcacec51fa03d19
Augusto Leandro <106690808+LeandroSSA@users.noreply.github.com>
2024-04-26 12:52:37 -0300

    Commit
    Ajustando configuração

---
+++ BusTicketNow_DEV/login.js
@@ -0,0 +1,6 @@
+function entrar() {
+    // Aqui você pode adicionar lógica de validação de login (ex: verificar nome de usuário e senha)
+    
+    // Redirecionar para a tela principal do sistema (home.html) após o login
+    window.location.href = "TelaInicial.html";
+}
